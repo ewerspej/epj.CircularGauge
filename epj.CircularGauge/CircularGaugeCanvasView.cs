02e29c548588efe1ef0da6462c2a88ce7d88361a
ewerspej <ewerspej@tcd.ie>
2021-05-08 17:17:44 +0200

    refactored CircularGaugeCanvasView a bit and added additional Properties for later use

--- epj.CircularGauge/CircularGaugeCanvasView.cs
+++ epj.CircularGauge/CircularGaugeCanvasView.cs
@@ -8,40 +8,61 @@ namespace epj.CircularGauge
 {
     internal sealed class CircularGaugeCanvasView : SKCanvasView
     {
+        #region Private Fields
+
+        private SKImageInfo _info;
+        private SKSurface _surface;
+        private SKCanvas _canvas;
+        private SKRect _drawRect;
+
+        #endregion
+
+        #region Properties
         internal float StartAngle { get; set; } = 45.0f;
         internal float SweepAngle { get; set; } = 270.0f;
         internal float GaugeWidth { get; set; } = 40.0f;
         internal Color GaugeColor { get; set; } = Color.Red;
         internal List<Color> GaugeGradientColors { get; set; } = new List<Color>();
+        internal float RangeStart { get; set; } = 0.0f;
+        internal float RangeEnd { get; set; } = 100.0f;
+        internal float Value { get; set; } = 50.0f;
+
+        #endregion
 
         protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
         {
-            var info = e.Info;
-            var surface = e.Surface;
-            var canvas = surface.Canvas;
+            _info = e.Info;
+            _surface = e.Surface;
+            _canvas = _surface.Canvas;
+            _canvas.Clear();
 
-            canvas.Clear();
-
-            var rect = new SKRect(100, 100, info.Width - 100, info.Height - 100);
+            //setup the rectangle which we will draw in
+            _drawRect = new SKRect(100, 100, _info.Width - 100, _info.Height - 100);
 
             //the coordinate system of SkiaSharp starts with 0 degrees at 3 o'clock (polar coordinates),
             //but we want 0 degrees at 6 o'clock, so we rotate everything by 90 degrees.
             var startAngle90 = StartAngle + 90.0f;
 
+            DrawGauge(startAngle90);
+        }
+
+        private void DrawGauge(float startAngle)
+        {
+            //draw gauge base
             using (var path = new SKPath())
             {
-                path.AddArc(rect, startAngle90, SweepAngle);
+                path.AddArc(_drawRect, startAngle, SweepAngle);
 
                 using (var paint = new SKPaint())
                 {
                     if (GaugeGradientColors?.Count > 0)
                     {
                         var colors = GaugeGradientColors.Select(color => color.ToSKColor()).ToArray();
-                        var center = new SKPoint(info.Rect.MidX, info.Rect.MidY);
+                        var center = new SKPoint(_info.Rect.MidX, _info.Rect.MidY);
 
-                        paint.Shader = SKShader.CreateSweepGradient(center: center, colors: colors, tileMode: SKShaderTileMode.Clamp, startAngle: 0.0f, endAngle: SweepAngle)
-                                               .WithLocalMatrix(SKMatrix.CreateRotationDegrees(startAngle90, center.X, center.Y));
-
+                        paint.Shader = SKShader.CreateSweepGradient(center: center, colors: colors,
+                                tileMode: SKShaderTileMode.Clamp, startAngle: 0.0f, endAngle: SweepAngle)
+                            .WithLocalMatrix(SKMatrix.CreateRotationDegrees(startAngle, center.X, center.Y));
                     }
                     else
                     {
@@ -51,7 +72,7 @@ namespace epj.CircularGauge
                     paint.IsAntialias = true;
                     paint.Style = SKPaintStyle.Stroke;
                     paint.StrokeWidth = GaugeWidth;
-                    canvas.DrawPath(path, paint);
+                    _canvas.DrawPath(path, paint);
                 }
             }
         }
