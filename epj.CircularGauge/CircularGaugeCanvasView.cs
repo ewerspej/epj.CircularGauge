604d741b409b953745be50780a40b1f5340cbc15
ewerspej <ewerspej@tcd.ie>
2021-05-08 11:02:15 +0200

    first steps drawing an arc with SkiaSharp

---
+++ epj.CircularGauge/CircularGaugeCanvasView.cs
@@ -0,0 +1,41 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace epj.CircularGauge
+{
+    public class CircularGaugeCanvasView : SKCanvasView
+    {
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            var info = e.Info;
+            var surface = e.Surface;
+            var canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            var rect = new SKRect(100, 100, info.Width - 100, info.Height - 100);
+            var startAngle = 45.0f;
+            var sweepAngle = 90.0f;
+
+            //canvas.DrawOval(rect, new SKPaint
+            //{
+            //    Color = Color.Black.ToSKColor(),
+            //    IsAntialias = true,
+            //    Style = SKPaintStyle.Fill
+            //});
+
+            using (var path = new SKPath())
+            {
+                path.AddArc(rect, startAngle, sweepAngle);
+                canvas.DrawPath(path, new SKPaint
+                {
+                    Color = Color.Red.ToSKColor(),
+                    IsAntialias = true,
+                    Style = SKPaintStyle.Stroke,
+                    StrokeWidth = 25
+                });
+            }
+        }
+    }
+}
